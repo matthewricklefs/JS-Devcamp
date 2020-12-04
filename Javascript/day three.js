ef90b6c8085c1c502234fab9a7b4f4896fe71bde
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-04 18:39:03 +0000

    using 'this' in JS programs

--- Javascript/day three.js
+++ Javascript/day three.js
@@ -36,7 +36,34 @@
 
 // Introduction to the 'this' Keyword in JavaScript //
 
-$('.btn-guide').click(function(event) {
-    event.preventDefault();
-    console.log($(this));
-});
+// $('.btn-guide').click(function(event) {
+//     event.preventDefault();
+//     console.log($(this));
+// });
+
+
+// How to Use the 'this' Keyword in JavaScript Programs
+
+var guide = {
+  title: 'Guide to Programming', //
+  content: 'Content goes here...',
+  visibleToUser: function (viewingUserRole) {
+    if (viewingUserRole === 'paid') {
+      return true;
+    }
+  },
+  renderContent: function(userRole) {
+    if (this.visibleToUser(userRole)) {
+      console.log(this.title + " - " + this.content);
+    } else {
+      this.content = '';
+      console.log(this.title + " - " + this.content);
+    }
+  }
+}
+
+user = { role: 'paid' };
+guide.renderContent(user.role);
+
+//// Section Introduction: Introduction to JavaScript Arrays//
+
