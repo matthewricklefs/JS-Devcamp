150a3e762815dda9664734b8c56c3fe3d292d7a4
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-04 21:16:49 +0000

    While and Do/While Loops in JS

--- Javascript/day three.js
+++ Javascript/day three.js
@@ -115,6 +115,39 @@
 
 //Guide to For Loops in JavaScript//
 
+// var players = [
+//   'Altuve',
+//   'Bregman',
+//   'Correa',
+//   'Springer'
+// ];
+
+// for (player in players) {
+//   console.log(players[player]);
+// }
+
+// for (var i = 0; i < players.length; i++) {
+//   console.log(players[i]);
+// }
+
+// players.forEach(function(element) {
+//   console.log(element);
+// });
+
+//How to Loop Through a JavaScript Object//
+
+// var student = {
+//   name: 'Kristine',
+//   age: 12,
+//   city: 'Scottsdale'
+// };
+
+// for (var key in student) {
+//   console.log(key + " => " + student[key]);
+// }
+
+// Guide to While and Do/While Loops in JavaScript //
+
 var players = [
   'Altuve',
   'Bregman',
@@ -122,26 +155,16 @@ var players = [
   'Springer'
 ];
 
-for (player in players) {
-  console.log(players[player]);
-}
-
-for (var i = 0; i < players.length; i++) {
+var i = 0;
+while (i < players.length) {
   console.log(players[i]);
+  i++;
 }
 
-players.forEach(function(element) {
-  console.log(element);
-});
+var i = 21;
+do {
+  console.log(players[i]);
+  i++;
+} while (i < players.length)
 
-//How to Loop Through a JavaScript Object//
-
-var student = {
-  name: 'Kristine',
-  age: 12,
-  city: 'Scottsdale'
-};
-
-for (var key in student) {
-  console.log(key + " => " + student[key]);
-}
+//   //
