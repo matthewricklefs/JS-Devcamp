3719b1bda5c63bfae4533839e1b0928beda5f8dc
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-07 23:06:54 +0000

    the Spread Operator in JS

--- Javascript/Average-From-Array.js
+++ Javascript/Average-From-Array.js
@@ -99,29 +99,64 @@
 
 //How to Pass JavaScript Objects as Function Arguments by Leveraging Deconstruction//
 
-const user = {
-  name: 'Kristine',
-  email: 'kristine@devcamp.com'
-}
+// const user = {
+//   name: 'Kristine',
+//   email: 'kristine@devcamp.com'
+// }
 
-const renderUser = ({ name, email }) => {
-  console.log(`${name}: ${email}`);
-}
+// const renderUser = ({ name, email }) => {
+//   console.log(`${name}: ${email}`);
+// }
 
-renderUser(user);
+// renderUser(user);
 
 // Guide to Adding Default Object Values to JavaScript Function Arguments //
 
-const blog = {
-  title: 'My great post',
-  summary: 'Summary of my post'
+// const blog = {
+//   title: 'My great post',
+//   summary: 'Summary of my post'
+// }
+
+// const openGraphMetadata = ({ title, summary = 'A DailySmarty Post' }) => {
+//   console.log(`
+//     og-title=${title}
+//     og-description=${summary}
+//   `);
+// }
+
+// openGraphMetadata(blog);
+
+
+//// Guide to the JavaScript Spread Operator ////
+
+// Combining Arrays
+let shoppingCart = [345, 375, 765, 123];
+let newItems = [98, 123];
+
+shoppingCart.push(...newItems);
+console.log(shoppingCart); // [345, 375, 765, 123, 98, 123]
+
+// Copying Arrays
+const shoppingCart = [345, 375, 765, 123];
+const updatedCart = [...shoppingCart];
+updatedCart.push(5);
+console.log(updatedCart);
+console.log(shoppingCart);
+
+const orderTotals = [1, 5, 1, 10, 2, 3];
+console.log(Math.max(...orderTotals));
+
+// [1, 5, 1, 10, 2, 3]
+// ...[1, 5, 1, 10, 2, 3]
+// 1, 5, 1, 10, 2, 3
+
+const { starter, closer, ...relievers } = {
+  starter: 'Verlander',
+  closer: 'Giles',
+  relief_1: 'Morton',
+  relief_2: 'Gregerson'
 }
 
-const openGraphMetadata = ({ title, summary = 'A DailySmarty Post' }) => {
-  console.log(`
-    og-title=${title}
-    og-description=${summary}
-  `);
-}
-
-openGraphMetadata(blog);
+console.log(starter);
+console.log(closer);
+console.log(relievers);
