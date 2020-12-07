97f8605d59277be3041c369352a6547df011b0ea
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-07 21:52:35 +0000

    Implementing Array Destructuring

--- Javascript/Average-From-Array.js
+++ Javascript/Average-From-Array.js
@@ -67,18 +67,32 @@
 
 // How to Swap Variable Values in JavaScript with Variable Deconstruction//
 
-let playerOne = 'Tiffany';
-let playerTwo = 'Kristine';
+// let playerOne = 'Tiffany';
+// let playerTwo = 'Kristine';
 
-let tempPlayerOne = playerOne;
-let tempPlayerTwo = playerTwo;
+// let tempPlayerOne = playerOne;
+// let tempPlayerTwo = playerTwo;
 
-playerOne = tempPlayerTwo;
-playerTwo = tempPlayerOne;
+// playerOne = tempPlayerTwo;
+// playerTwo = tempPlayerOne;
 
-[playerOne, playerTwo] = [playerTwo, playerOne];
+// [playerOne, playerTwo] = [playerTwo, playerOne];
 
-console.log(`
-Player One: ${playerOne}
-Player Two: ${playerTwo}
-`);
+// console.log(`
+// Player One: ${playerOne}
+// Player Two: ${playerTwo}
+// `);
+
+//// How to Implement Array Destructuring in JavaScript ////
+
+const apiList = [
+  'https://api.dailysmarty.com/posts',
+  'https://api.github.com/users/jordanhudgens/repos',
+  'https://api.github.com/users/jordanhudgens'
+]
+
+const [posts, repos, profile] = apiList;
+
+console.log(posts);
+console.log(repos);
+console.log(profile);
