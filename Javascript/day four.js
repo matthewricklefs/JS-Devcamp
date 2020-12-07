3235996db207042aacfc5caeeeb8fba8f1a3d421
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-07 17:50:53 +0000

    Intergrating Conditionals in JS  w/ Ternary Operators
    Arrow Functions

--- Javascript/day four.js
+++ Javascript/day four.js
@@ -33,15 +33,50 @@
 // Guide to Backtick string interpolation in Javascript //
 
 
-const lyrics = 'these are my lyrics'
-console.log( "I'm + lyrics")
+// const lyrics = 'these are my lyrics'
+// console.log( "I'm + lyrics")
 
-//backtick method w/ declared variable
-const lyrics = 'these are my lyrics'
-console.log(`"I'm ${lyrics}`")
+// //backtick method w/ declared variable
+// const lyrics = 'these are my lyrics'
+// console.log(`"I'm ${lyrics}`")
 
-// backtick method w/ numbers 
-const lyrics = 'these are my lyrics'
-console.log(`"I'm ${2 + 2}`")
+// // backtick method w/ numbers 
+// const lyrics = 'these are my lyrics'
+// console.log(`"I'm ${2 + 2}`")
 
 //can place multiple variables inside of the Curly Brackets
+
+
+//Integrating Conditionals into JavaScript Strings with Ternary Operators//
+
+
+// const page = 'about';
+// console.log(`class=${ page === 'Home' ? 'master-layout' : 'secondary-layout' }`);
+
+//// Introduction to Arrow Functions in JavaScript ////
+
+// Same function written as function declaration
+// function fullName (fName, lName) { 
+//   console.log(`${lName}, ${fName}`);
+// }
+// fullName('Tiffany', 'Hudgens');
+
+// // Same function written as function expression
+// fullName = (fName, lName) => { 
+//   console.log(`${lName}, ${fName}`);
+// }
+// fullName('Kristine', 'Hudgens');
+
+// // Basic arrow function
+// helloWorld = () => { console.log("Hi there"); }
+// helloWorld();
+
+// // Arrow function with shorthand function argument for single arguments
+// firstName = fname => { console.log(fname.toUpperCase()); }
+// firstName('Jordan');
+
+// // Arrow function with multiple arguments
+// fullName = (fName, lName) => { console.log(`${lName}, ${fName}`); }
+// fullName('Kristine', 'Hudgens');
+
+////  ////
