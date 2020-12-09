926f9c07037c88657c06f33b9d2c1c9e652a3f41
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-09 16:41:57 +0000

    Added Second Solution

--- Javascript/RemoveSpecificIndex.js
+++ Javascript/RemoveSpecificIndex.js
@@ -1,3 +1,4 @@
+// First Solution
 function removeByIndex(arr, specifiedIndec) {
   const removed = []
   
@@ -9,3 +10,16 @@ function removeByIndex(arr, specifiedIndec) {
 }
 
 console.log(removeByIndex(['a', 'b', 'c', 'd', 'e', 'f', 'f', 'e', 'f'], [1, 5]))
+
+//Second Solution
+// Write a program to extract out the values at specified indeces from an array.
+// removeByIndex(['a','b','c','d','f','f','e','f'], [1, 5]) => ['b', 'f']
+function removeByIndex(arr, specifiedIndex){
+  // const removed = []
+  // specifiedIndex.forEach(idx => {
+  //   removed.push(arr[idx])
+  // })
+  // return removed
+  return specifiedIndex.map(idx => arr[idx])
+}
+console.log(removeByIndex(['a','b','c','d','f','f','e','f'], [1, 5]))
