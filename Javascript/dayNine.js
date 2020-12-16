a72757f4246f578bce5c1b81d88407b17d274991
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-16 17:13:35 +0000

    process JS from a file in Browser// Creating Custom Code Snippets in VS code

--- Javascript/dayNine.js
+++ Javascript/dayNine.js
@@ -18,28 +18,89 @@
 
 // How to Run JavaScript Scripts in the Browser Console //
 
-function fullName(first, last) {
-  return `${first} ${last}`;
+// function fullName(first, last) {
+//   return `${first} ${last}`;
+// }
+
+// fullName('Kristine', 'Hudgens');
+
+
+// newFullName = (first, last) => {
+//   return `${first} ${last}`;
+// }
+
+// newFullName('Kristine', 'Hudgens');
+
+// // Comparison of Chrome's Command Line API vs Vanilla JavaScript //
+
+// // Source page: http://www.dailysmarty.com/
+
+// // Console with Command Line API / Firebug
+// $('.image-item')
+// $('.image-item').first // returns function
+// $('.image-item').first()
+// $('.image-item').first().hide()
+
+// // Compared with
+// document.getElementsByClassName('post-item-wrapper')[0].style.display = 'none';
+
+//How to Process JavaScript from a File in the Browser//
+
+// <!DOCTYPE html>
+// <html lang='en'>
+// <head>
+//   <meta charset='UTF-8'>
+//   <title></title>
+// </head>
+// <body>
+//   <h1>Hi there</h1>
+// </body>
+// <script>
+//   fullName = (first, last) => {
+//     return `${first} ${last}`;
+//   }
+
+//   alert(fullName('Kristine', 'Hudgens'));
+// </script>
+// </html>
+
+//How to Create Custom Code Snippets in VS Code//
+{
+    "Boilerplate": {
+        "prefix": "html5",
+        "body": [
+            "<!DOCTYPE html>",
+            "<html lang='en'>",
+            "<head>",
+            "  <meta charset='UTF-8'>",
+            "  <title></title>",
+            "</head>",
+            "<body>",
+            "  $0",
+            "</body>",
+            "</html>"
+        ],
+        "description": "Generates HTML5 Boilerplate"
+    },
+    "Bullets": {
+        "prefix": "bullets",
+        "body": [
+            "<ul>",
+            "  <li>$1</li>",
+            "</ul>"
+        ],
+        "description": "Adds bullet point list to HTML file"
+    }
 }
 
-fullName('Kristine', 'Hudgens');
 
-
-newFullName = (first, last) => {
-  return `${first} ${last}`;
+{
+    "Print to console": {
+        "prefix": "log",
+        "body": [
+            "console.log('$1');",
+            "$2"
+        ],
+        "description": "Log output to console"
+    }
 }
-
-newFullName('Kristine', 'Hudgens');
-
-// Comparison of Chrome's Command Line API vs Vanilla JavaScript //
-
-// Source page: http://www.dailysmarty.com/
-
-// Console with Command Line API / Firebug
-$('.image-item')
-$('.image-item').first // returns function
-$('.image-item').first()
-$('.image-item').first().hide()
-
-// Compared with
-document.getElementsByClassName('post-item-wrapper')[0].style.display = 'none';
