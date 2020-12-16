2fd8e619e1a737d29988243adc02d3059478d4d7
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-16 18:06:46 +0000

    Pretty Price method in JS

--- Javascript/dayNine.js
+++ Javascript/dayNine.js
@@ -65,42 +65,52 @@
 // </html>
 
 //How to Create Custom Code Snippets in VS Code//
-{
-    "Boilerplate": {
-        "prefix": "html5",
-        "body": [
-            "<!DOCTYPE html>",
-            "<html lang='en'>",
-            "<head>",
-            "  <meta charset='UTF-8'>",
-            "  <title></title>",
-            "</head>",
-            "<body>",
-            "  $0",
-            "</body>",
-            "</html>"
-        ],
-        "description": "Generates HTML5 Boilerplate"
-    },
-    "Bullets": {
-        "prefix": "bullets",
-        "body": [
-            "<ul>",
-            "  <li>$1</li>",
-            "</ul>"
-        ],
-        "description": "Adds bullet point list to HTML file"
-    }
-}
+// {
+//     "Boilerplate": {
+//         "prefix": "html5",
+//         "body": [
+//             "<!DOCTYPE html>",
+//             "<html lang='en'>",
+//             "<head>",
+//             "  <meta charset='UTF-8'>",
+//             "  <title></title>",
+//             "</head>",
+//             "<body>",
+//             "  $0",
+//             "</body>",
+//             "</html>"
+//         ],
+//         "description": "Generates HTML5 Boilerplate"
+//     },
+//     "Bullets": {
+//         "prefix": "bullets",
+//         "body": [
+//             "<ul>",
+//             "  <li>$1</li>",
+//             "</ul>"
+//         ],
+//         "description": "Adds bullet point list to HTML file"
+//     }
+// }
 
 
-{
-    "Print to console": {
-        "prefix": "log",
-        "body": [
-            "console.log('$1');",
-            "$2"
-        ],
-        "description": "Log output to console"
-    }
+// {
+//     "Print to console": {
+//         "prefix": "log",
+//         "body": [
+//             "console.log('$1');",
+//             "$2"
+//         ],
+//         "description": "Log output to console"
+//     }
+// }
+
+// Build a Pretty Price Method in JavaScript //
+
+//define function that takes in two arguments
+const prettyPrice = (grossPrice, extension) => {
+  // return parseInt
+  return parseInt(grossPrice)+ extension
+
 }
+console.log(prettyPrice(2.25, .90))
