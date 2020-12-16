d58e8ac7e85935e04da59cd46dd33bc6537940e4
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-16 16:45:58 +0000

    JS scripts in Browser Console / Chrome's Command Line API vs Vanilla JS

--- Javascript/dayNine.js
+++ Javascript/dayNine.js
@@ -1,17 +1,45 @@
 // Function using JS to convert a given numver into an array of digits. //
 
-//1st Solution//
+// //1st Solution//
 
-const numToArray = (number) => {
-  let array = number.toString().split("");
-  	return array.map(i => parseInt(i));
+// const numToArray = (number) => {
+//   let array = number.toString().split("");
+//   	return array.map(i => parseInt(i));
+// }
+
+// console.log(numToArray(123456));
+// console.log(numToArray(5432));
+
+// //2nd Solution//
+// function numberToArray(num) {
+//   return [...`${num}`].map(i => parseInt(i))
+// }
+// console.log(numberToArray(12345))
+
+// How to Run JavaScript Scripts in the Browser Console //
+
+function fullName(first, last) {
+  return `${first} ${last}`;
 }
 
-console.log(numToArray(123456));
-console.log(numToArray(5432));
+fullName('Kristine', 'Hudgens');
 
-//2nd Solution//
-function numberToArray(num) {
-  return [...`${num}`].map(i => parseInt(i))
+
+newFullName = (first, last) => {
+  return `${first} ${last}`;
 }
-console.log(numberToArray(12345))
+
+newFullName('Kristine', 'Hudgens');
+
+// Comparison of Chrome's Command Line API vs Vanilla JavaScript //
+
+// Source page: http://www.dailysmarty.com/
+
+// Console with Command Line API / Firebug
+$('.image-item')
+$('.image-item').first // returns function
+$('.image-item').first()
+$('.image-item').first().hide()
+
+// Compared with
+document.getElementsByClassName('post-item-wrapper')[0].style.display = 'none';
