5eb1b2e4614ca37e6a221de481e1157bce114ed5
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-17 21:45:19 +0000

    UPDATED .pop, .shift, .slice in JS

--- Javascript/JS-Day10/removeElements.js
+++ Javascript/JS-Day10/removeElements.js
@@ -1,6 +1,23 @@
-function arrayPop() {
-  arr.pop()
-  arr.shift()
-  return arr
+
+let arr = ['ughhh', 'goood', 'another good one', 'uggg']
+
+//First Solution
+// const arrayPopper = () => {
+//     arr.shift()
+//     arr.pop()
+//     return arr
+// }
+
+// console.log(arrayPopper);
+
+
+//Second Solution
+const removeFirstAndLast = arr => {
+    if (arr.length >= 3) {
+        return arr.slice(1, -1);
+    } else {
+        throw "You need at least three elements in the array.";
+    } 
 }
-console.log(arrayPop(arr))
+
+removeFirstAndLast(arr)
