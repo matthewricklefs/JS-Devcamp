316db584e9f3414d7fe2c861b35afc358d1d4a7b
Matthew Ricklefs <ricklefsmatthew1003@gmail.com>
2020-12-17 23:04:04 +0000

    Ex. of Console Function Methods

---
+++ Javascript/JS-Day10/consoleMethods.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    
+</body>
+
+<script>
+    //creating object
+
+    // const someObj = {
+    //     name: 'Kristine'
+    // }
+    // console.log('Hi', someObj, 'After object');
+
+    //console table example
+
+    // const playerNames = [
+    //         { name: 'Altuve', pos: '2b' },
+    //         { name: 'Correa', pos: 'ss' },
+    //         { name: 'Bregman', pos: '3b' },
+    // ]  
+    // console.table(playerNames)
+
+    //console error example
+
+    console.error('oops');
+
+    //console warn example
+
+    console.warn('Something is about to go wrong')
+
+    
+
+
+</script>
+
+</html>
